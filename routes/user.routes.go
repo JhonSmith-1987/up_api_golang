2f887d81b1775659fe2ef4b636f0d66cb6211937
JhonSmith-1987 <77317038+JhonSmith-1987@users.noreply.github.com>
2022-12-09 23:04:55 -0500

    create request post

--- routes/user.routes.go
+++ routes/user.routes.go
@@ -1,16 +1,32 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"up_api_golang/db"
+	"up_api_golang/models"
+)
 
 func GetUsersHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("get users"))
 }
+
 func GetUserHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("get user"))
 }
+
 func PostUserHandler(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("post"))
+	var user models.Users
+	json.NewDecoder(req.Body).Decode(&user)
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest) // 400
+		res.Write([]byte(err.Error()))
+	}
+	json.NewEncoder(res).Encode(createdUser)
 }
+
 func DeleteUsersHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("delete"))
 }
